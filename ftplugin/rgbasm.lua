98e5c4b76fe7b1f112841ada96512500879359d3
jannschu <jannik.schuerg@posteo.de>
2025-12-04 17:50:45 +0100

    Use heuristic for file type detection for common file extensions

--- ftplugin/rgbasm.lua
+++ ftplugin/rgbasm.lua
@@ -3,14 +3,9 @@ vim.treesitter.start()
 -- Set comment string for commenting/uncommenting
 vim.bo.commentstring = "; %s"
 
--- Set up indentation
--- FIXME: Should not be hardcoded?
 vim.bo.expandtab = true
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
+vim.bo.shiftwidth = 4
 
--- Enable autoindent but disable smart/cindent to let indentexpr work
 vim.bo.autoindent = true
 vim.bo.smartindent = false
 vim.bo.cindent = false
--- lua/rgbds/filetype.lua
+++ lua/rgbds/filetype.lua
@@ -1,10 +1,74 @@
 local M = {}
 
+local function buffer_contains_rgbasm(bufnr)
+	bufnr = bufnr or 0
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 100, false)
+	local content = table.concat(lines, "\n")
+
+	local patterns = {
+		"SECTION%s+[\"']", -- SECTION "Name"
+		"%sROM[0X]", -- ROM0, ROMX
+		"%sWRAM[0X]", -- WRAM0, WRAMX
+		"%sSRAM", -- SRAM
+		"%sHRAM", -- HRAM
+		"hardware%.inc", -- hardware.inc include
+		"ldh%s+", -- ldh instruction
+		"%[hl[%+%-]%]", -- [hl+] or [hl-]
+		"rLCDC", -- hardware register
+		"rSTAT",
+		"rSCY",
+		"rSCX",
+	}
+
+	for _, pattern in ipairs(patterns) do
+		if content:match(pattern) then
+			return true
+		end
+	end
+
+	return false
+end
+
+local function has_rgbds_makefile(bufnr)
+	local path = vim.api.nvim_buf_get_name(bufnr or 0)
+	if path == "" then
+		-- buffer has no name
+		return false
+	end
+	local makefiles = vim.fs.find(
+		{ "Makefile", "makefile", "GNUmakefile", "hardware.inc" },
+		{ upward = true, type = "file", path = vim.fs.dirname(path), limit = 5 }
+	)
+
+	if #makefiles == 0 then
+		return false
+	end
+
+	for p in makefiles do
+		if vim.fs.basename(p) == "hardware.inc" then
+			return true
+		end
+	end
+
+	local file = io.open(makefiles[1], "r")
+	if not file then
+		return false
+	end
+
+	local content = file:read("*a")
+	file:close()
+
+	-- Nach rgbasm, rgblink, rgbfix suchen
+	return content:match("rgbasm") or content:match("rgblink") or content:match("rgbfix") ~= nil
+end
+
 ---@param path string
----@param bufnr number
+---@param bufnr number|nil
 function M.detect(path, bufnr)
-	-- TODO: refine this
-	return "rgbasm"
+	if buffer_contains_rgbasm(bufnr) or has_rgbds_makefile(bufnr) then
+		return "rgbasm"
+	end
+	return nil
 end
 
 return M
